7c40829d3f0274b30d9798c84d9c36aceaa61d59
pickleservices <a.alexandrov@yahoo.com>
2024-07-12 19:39:18 +0100

    changes to javascript
    Made functions less clunky, added more styling, and created ads

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*
+
 function createPiped() {
     var win = window.open('about:blank#ff');
     win.document.body.style.margin = '0';
@@ -88,20 +90,7 @@ function createPiped() {
     iframe.src = 'https://roberttbs.github.io/slowroads/';
     win.document.body.appendChild(iframe);
     }
-    
-  function createUno() {
-    var win = window.open('about:blank#ff');
-    win.document.body.style.margin = '0';
-    win.document.body.style.height = '100vh';
-    var iframe = win.document.createElement('iframe');
-    iframe.style.border = 'none';
-    iframe.style.width = '100%';
-    iframe.style.height = '100%';
-    iframe.style.margin = '0';
-    iframe.src = 'https://oskar-codes.github.io/uno-online/';
-    win.document.body.appendChild(iframe);
-    }
-    
+   
   function createEuro() {
     var win = window.open('about:blank#ff');
     win.document.body.style.margin = '0';
@@ -166,6 +155,53 @@ function createPiped() {
     iframe.src = 'https://mathadventure1.github.io/slope/slope/index.html';
     win.document.body.appendChild(iframe);
     }
+  
+  */
+
+    function openIframe(url) {
+      // Open a new window with about:blank#ff
+      const newWindow = window.open('about:blank#ff');
+  
+      if (newWindow) {
+          // Wait a short moment before writing the content to ensure the new window is ready
+          setTimeout(() => {
+              // Write the HTML content to the new window
+              newWindow.document.open();
+              newWindow.document.write(`
+                  <!DOCTYPE html>
+                  <html lang="en">
+                  <head>
+                      <meta charset="UTF-8">
+                      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                      <title>Google Drive - My Drive</title>
+                      <link rel="icon" type="image/x-icon" href="../favicon.ico">
+                      <style>
+                          html, body {
+                              margin: 0;
+                              padding: 0;
+                              width: 100%;
+                              height: 100%;
+                              overflow: hidden;
+                          }
+                          iframe {
+                              width: 100%;
+                              height: 100%;
+                              border: none;
+                          }
+                      </style>
+                  </head>
+                  <body>
+                      <iframe src="${url}" frameborder="0"></iframe>
+                  </body>
+                  </html>
+              `);
+              newWindow.document.close();
+          }, 100);
+      } else {
+          alert('Please allow popups for this website.');
+      }
+  }
+  
   
   
     
